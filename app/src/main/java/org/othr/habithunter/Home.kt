17002fbfba4ea03e51c220ebcbaa0f302fbb0a6d
AlexW850 <alexander.wastl@st.oth-regensburg.de>
2022-05-13 22:01:08 +0200

    Initial commit with bottom navigation including three basic fragments (dashboard, statistics, profile) and changed theme color

---
+++ app/src/main/java/org/othr/habithunter/Home.kt
@@ -0,0 +1,35 @@
+package org.othr.habithunter
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import org.othr.habithunter.databinding.ActivityHomeBinding
+
+class Home : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_dashboard, R.id.navigation_stats, R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/org/othr/habithunter/ui/stats/StatsViewModel.kt
@@ -0,0 +1,13 @@
+package org.othr.habithunter.ui.stats
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class StatsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
